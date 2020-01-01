ed4fddf17bf632e44378ce60693e294288430345
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2020-01-01 15:51:05 -0800

    First commit

---
+++ Assets/Scripts/PuzzleBoardManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleBoardManager : MonoBehaviour
+{
+  //[SerializeField]
+  public int rows = 4;
+  //[SerializeField]
+  public int cols = 4;
+  //[SerializeField]
+  private float tileSize = 0.9f;
+  public Color startColor = new Color(255f/255f, 0/255f, 0/255f);
+  public Color endColor = new Color(0/255f, 0/255f, 255f/255f);
+  public Color[,] colorArray;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    GenerateColorArray();
+    GenerateGrid();
+  }
+
+  private void GenerateGrid()
+  {
+    GameObject referenceTile = (GameObject)Instantiate(Resources.Load("HexTile"));
+    
+    for (int row = 0; row < rows; row++)
+    {
+      for (int col = 0; col < cols; col++)
+      {
+        GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+
+        float posX = col * tileSize;
+        if (row % 2 == 0) {
+          posX -= 0.3f;
+        }
+
+        float posY = row * -tileSize;
+
+        tile.transform.position = new Vector2(posX, posY);
+        tile.transform.Rotate(0.0f, 0.0f, 50.0f, Space.World);
+
+        var tileRenderer = tile.GetComponent<Renderer>();
+        Color tileColor = colorArray[row, col];
+        tileRenderer.material.color = tileColor;
+      }
+    }
+
+    Destroy(referenceTile);
+
+    float gridWidth = cols * tileSize;
+    float gridHeight = rows * tileSize;
+
+    //pivot point for tiles is in the center
+    transform.position = new Vector2(-gridWidth/2 + tileSize/2, (gridHeight/2 - tileSize/2)-2);
+  }
+
+  public void GenerateColorArray()
+  {
+    // currently using only half of the colors intended
+    Color tileWidth = ((endColor - startColor)/(cols * rows));
+
+    colorArray = new Color[rows, cols];
+    int x = 0;
+
+    for (int i = 0; i < rows; i++)
+    {
+      for (int j = 0; j < cols; j++)
+      {
+        colorArray[i,j] = (startColor + (tileWidth * x));
+        x += 1;
+      }
+    }
+  }
+}
---
+++ Assets/Scripts/PuzzlePieceManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePieceManager : MonoBehaviour
+{
+  private int puzzleRows;
+  private int puzzleCols;
+  private Color startColor;
+  private Color endColor;
+  private Color[] colorArray;
+  private float tileSize = 1f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    GenerateVariables();
+    GenerateColorArray();
+    GenerateTiles();
+  }
+
+  private void GenerateVariables()
+  {
+    GameObject puzzleBoard = GameObject.Find("PuzzleBoard");
+
+    PuzzleBoardManager existingBoard = puzzleBoard.GetComponent<PuzzleBoardManager>();
+
+    puzzleRows = existingBoard.rows;
+    puzzleCols = existingBoard.cols;
+    startColor = existingBoard.startColor;
+    endColor = existingBoard.endColor;
+  }
+
+  private void GenerateColorArray()
+  {
+    int tileCount = (puzzleCols * puzzleRows);
+    Color tileWidth = ((endColor - startColor)/tileCount);
+
+    colorArray = new Color[tileCount];
+
+    for (int i = 0; i < tileCount; i++)
+    {
+      colorArray[i] = (startColor + (tileWidth * i));
+    }
+  }
+
+  private void GenerateTiles()
+  {
+    GameObject referenceTile = (GameObject)Instantiate(Resources.Load("SquareTile"));
+
+      for (int i = 0; i < (puzzleCols * puzzleRows); i++)
+      {
+        GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+
+        float posX;
+        float posY;
+        if (i < (puzzleCols * puzzleRows)/2)
+        {
+          posX = i * tileSize;
+          posY = 5;
+        } else
+        {
+          posX = (i - (puzzleCols * puzzleRows)/2) * tileSize;
+          posY = 4;
+        }
+
+        tile.transform.position = new Vector2(posX, posY);
+
+        var tileRenderer = tile.GetComponent<Renderer>();
+
+        int end = colorArray.Length;
+        int start = 0;
+        int randomIndex = Random.Range(start, end);
+        Color tileColor = colorArray[randomIndex];
+        //colorArray.Controls.RemoveAt(randomIndex);
+        //colorArray = colorArray.Except(new Color[]{tileColor}).ToArray();
+
+        tileRenderer.material.color = tileColor;
+      }
+
+    Destroy(referenceTile);
+
+    float gridWidth = ((puzzleCols * puzzleRows)/2) * tileSize;
+    float gridHeight = tileSize * 2;
+
+    transform.position = new Vector2(-gridWidth/2 + tileSize/2, (gridHeight/2 - tileSize/2)-2);
+
+
+  }
+}
